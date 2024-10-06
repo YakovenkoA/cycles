93b92bd07fea1f8f9d2a3216b57b83d24cf72299
aleks <aleksandrakovenko21@gmail.com>
2024-10-06 20:32:32 +0500

    cycle

---
+++ pythonProject1/cycles.py
@@ -0,0 +1,33 @@
+for i in range(6):
+    print(i)
+
+print("Next")
+
+for i in range(1, 6):
+    print(i)
+
+print("Next")
+
+for i in range(1, 6, 2):
+    print(i)
+
+print("Next")
+
+for i in range(1, 6, 2):
+    print(i)
+
+print("Next")
+
+#word = "Hallo, world!!!"
+#for q in word:
+    #print(q)
+
+print("Next")
+
+isHasCar = True
+
+while isHasCar:
+    if input("Enter data: ") == "Stop":
+        isHasCar = False
+
+
